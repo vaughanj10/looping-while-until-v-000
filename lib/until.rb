e64f7ccc3003baf833adc654e175a0225d5117de
James Vaughan <james.vaughan@flatironschool.com>
2019-07-16 18:17:32 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,13 @@
 def using_until
+<<<<<<< HEAD
   levitation_force = 6
   #your code here
     
+=======
+	     levitation_force = 6
+	     until levitation_force == 10
+	        puts "Wingardium Leviosa"
+	        levitation_force += 1
+	    end
+>>>>>>> dbcd8cfdddc8bdac071c5f8931923a4924b67891
 end
-
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,14 @@
 def using_while
   levitation_force = 6
 	
+<<<<<<< HEAD
   #your code here
+=======
+	while levitation_force < 10
+		puts "Wingardium Leviosa"
+		levitation_force += 1
+	end
+>>>>>>> dbcd8cfdddc8bdac071c5f8931923a4924b67891
 end
 
 
